16b4ecf46699e965c16e48d5300f90bd31d30097
Tommy Turcovsky <turcovsky.t@husky.neu.edu>
2020-04-16 18:50:52 -0400

    Mongo/Heroku Compatibility

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 // This is the default address for MongoDB.
 // Make sure MongoDB is running!
-const mongoEndpoint = 'mongodb://127.0.0.1/shortenUrl_app';
+const mongoEndpoint = process.env.MONGODB_URI || 'mongodb://127.0.0.1/shortenUrl_app';
 // useNewUrlParser is not required, but the old parser is deprecated
 mongoose.connect(mongoEndpoint, { useNewUrlParser: true });
 
